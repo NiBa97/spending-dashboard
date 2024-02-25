77a5b93d92403d0e6c072de1049aa7743fdd5649
NiBa97 <niba97@icloud.com>
2024-02-25 13:39:26 +0100

    feat: Basic Transaction Table Implementation

---
+++ src/pages/transactions/index.tsx
@@ -0,0 +1,15 @@
+// write me a small page component that displays the transactiontable
+
+import React, { useContext } from "react";
+import { DataContext } from "../../components/data_context";
+import TransactionTable from "../../components/transaction_table/TransactionTable";
+
+const TransactionTablePage = () => {
+  const { data } = useContext(DataContext);
+  return (
+    <div>
+      <TransactionTable />
+    </div>
+  );
+};
+export default TransactionTablePage;
